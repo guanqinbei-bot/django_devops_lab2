06971eb8f8bdbef27f3b4f3fe1fae201ddd40ed8
Winson Koon <kp.koon@help.edu.my>
2026-04-20 11:41:55 +0800

    Fix CSRF for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -120,3 +120,7 @@ import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://django-devops-lab2.onrender.com"
+]
